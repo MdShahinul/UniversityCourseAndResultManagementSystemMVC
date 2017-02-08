248ead1a1eeaf22d21e71def3e38635567c6020c
Md. Shahinul Islam <shahinul.cse@gmail.com>
2017-02-09 00:03:38 +0600

    Result Model, Gateway, Model
    Student Model, Gateway, Model
    Grade Model

--- UniversityCourseAndResultMangementSystem/Controllers/StudentResultController.cs
+++ UniversityCourseAndResultMangementSystem/Controllers/StudentResultController.cs
@@ -10,17 +10,25 @@ namespace UniversityCourseAndResultMangementSystem.Controllers
 {
     public class StudentResultController : Controller
     {
+
         RegisterStudentManager registerStudentManager = new RegisterStudentManager();
         StudentResultManager studentResultManager = new StudentResultManager();
+        [HttpGet]
         public ActionResult StudentResultSave()
         {
-            //ViewBag.registerList = registerStudentManager.GateRegistationNumberManager();
-            //ViewBag.gradeList = studentResultManager.GatAllGradeManager(); 
+            ViewBag.message = TempData["Msg"]; 
+            ViewBag.registerList = registerStudentManager.GateRegistationNumberManager();
+            ViewBag.gradeList = studentResultManager.GatAllGradeManager(); 
             return View();
         }
+
+        [HttpPost]
         public ActionResult StudentResultSave(StudentResultModel studentResultModel)
         {
-            return View();
+            ViewBag.message = studentResultManager.StudentResultSaveManager(studentResultModel);
+            TempData["Msg"] = ViewBag.message;
+            return RedirectToAction("StudentResultSave"); 
         }
-	}
+
+    }
 }
