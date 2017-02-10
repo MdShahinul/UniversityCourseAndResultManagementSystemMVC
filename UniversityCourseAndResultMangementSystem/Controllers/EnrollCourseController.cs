aeaf7ac41c4413e3deeca2049b67a484f267513b
Md. Shahinul Islam <shahinul.cse@gmail.com>
2017-02-11 02:34:29 +0600

    Enroll Course
    From upload

--- UniversityCourseAndResultMangementSystem/Controllers/EnrollCourseController.cs
+++ UniversityCourseAndResultMangementSystem/Controllers/EnrollCourseController.cs
@@ -4,19 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UniversityCourseAndResultMangementSystem.Manager;
+using UniversityCourseAndResultMangementSystem.Models;
 
 namespace UniversityCourseAndResultMangementSystem.Controllers
 {
     public class EnrollCourseController : Controller
     {
         RegisterStudentManager registerStudentManager = new RegisterStudentManager();
-       
+        CourseManager courseManager = new CourseManager();
+        EnrollCourseManager enrollCourseManager = new EnrollCourseManager();
+
+       [HttpGet]
         public ActionResult EnrollCourseSave()
         {
-            ViewBag.registerList = registerStudentManager.GateRegistationNumberManager(); 
+            ViewBag.message = TempData["Msg"];
+            ViewBag.registerList = registerStudentManager.GateRegistationNumberManager();
+            ViewBag.courseList=courseManager.GateAllCourseManager(); 
             return View();
         }
-
-       
+        [HttpPost]
+        public ActionResult EnrollCourseSave(EnrollCourseModel enrollCourseModel)
+        {
+            ViewBag.message = enrollCourseManager.GateAllEnrollCourseManager(enrollCourseModel);
+            TempData["Msg"] = ViewBag.message;
+            return RedirectToAction("EnrollCourseSave");
+        }
 	}
 }
---
+++ UniversityCourseAndResultMangementSystem/Manager/EnrollCourseManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultMangementSystem.Gateway;
+using UniversityCourseAndResultMangementSystem.Models;
+
+namespace UniversityCourseAndResultMangementSystem.Manager
+{
+    public class EnrollCourseManager
+    {
+        EnrollCourseGateway enrollCourseGateway = new EnrollCourseGateway();
+
+        public string GateAllEnrollCourseManager(EnrollCourseModel enrollCourseModel)
+        {
+            if (enrollCourseGateway.GateAllEnrollCourseGateway(enrollCourseModel) > 0)
+            {
+                return "Save Successfull"; 
+            }
+            else
+            {
+                return "Save Fail"; 
+            }
+        }
+    }
+}
